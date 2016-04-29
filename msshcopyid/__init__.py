523d1354c209f42491a9352137b0a5728fa5668f
Samuel Phan <samuel@quoonel.com>
2016-04-29 17:08:01 +0200

    Implement the copy of the SSH pub key

--- msshcopyid/__init__.py
+++ msshcopyid/__init__.py
@@ -5,9 +5,13 @@ import getpass
 import os
 import sys
 
+import paramiko
 
-DEFAULT_SSH_RSA = '~/.ssh/id_rsa'
-DEFAULT_SSH_DSA = '~/.ssh/id_dsa'
+
+DEFAULT_SSH_DIR = os.path.join(os.path.expanduser("~"), '.ssh')
+DEFAULT_SSH_RSA = os.path.join(DEFAULT_SSH_DIR, 'id_rsa')
+DEFAULT_SSH_DSA = os.path.join(DEFAULT_SSH_DIR, 'id_dsa')
+DEFAULT_KNOWN_HOSTS = os.path.join(DEFAULT_SSH_DIR, 'known_hosts')
 
 
 def main():
@@ -19,11 +23,14 @@ class Main(object):
 
     def __init__(self):
         self.args = None
+        self.pub_key = None
+        self.pub_key_content = None
 
     def main(self):
         # Parse input arguments
         self.args = self.parse_args(sys.argv)
 
+        # Check SSH key argument
         if not self.args.identity:
             self.args.identity = os.path.expanduser(DEFAULT_SSH_RSA)
             if not os.path.exists(self.args.identity):
@@ -32,7 +39,13 @@ class Main(object):
                     print('Error: Cannot find any SSH keys in {0} and {1}.'.format(DEFAULT_SSH_RSA, DEFAULT_SSH_DSA),
                           file=sys.stderr)
                     sys.exit(1)
+        self.pub_key = '{0}.pub'.format(self.args.identity)
 
+        # Read the public key
+        with open(self.pub_key) as fh:
+            self.pub_key_content = fh.read().strip()
+
+        # Check that a password is given
         if not self.args.password:
             if not sys.stdin.isatty():
                 self.args.password = sys.stdin.readline().strip()
@@ -41,7 +54,8 @@ class Main(object):
 
         # Copy the SSH keys to the hosts
         for host in self.args.hosts:
-            self.copy_ssh_keys(host)
+            # TODO: add the username
+            self.copy_ssh_keys(host, 'root', self.args.password)
 
     def parse_args(self, argv):
         parser = argparse.ArgumentParser(description='Massively copy SSH keys.')
@@ -55,6 +69,13 @@ class Main(object):
                                  'STDIN.')
         return parser.parse_args(argv[1:])
 
-    def copy_ssh_keys(self, host):
-        # TODO: implement it
-        print('I copy the SSH keys [{0}] to the host [{1}]...'.format(self.args.identity, host))
+    def copy_ssh_keys(self, host, username, password):
+        print('Copying the SSH public key [{0}] to the host [{1}]...'.format(self.pub_key, host))
+        with paramiko.SSHClient() as client:
+            client.set_missing_host_key_policy(paramiko.client.AutoAddPolicy())
+            client.load_host_keys(filename=DEFAULT_KNOWN_HOSTS)
+            client.connect(host, username=username, password=password, key_filename='/home/sphan/.ssh/id_rsa')
+            cmd = r'''mkdir -p ~/.ssh && chmod 700 ~/.ssh && \
+k='{0}' && if ! grep -qFx "$k" ~/.ssh/authorized_keys; then echo "$k" >> ~/.ssh/authorized_keys; fi'''\
+                    .format(self.pub_key_content)
+            client.exec_command(cmd)
