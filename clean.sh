a7f4e2773015fe9f16cb8f19a6de7312290fc26e
Samuel Phan <1516171+samuel-phan@users.noreply.github.com>
2016-05-04 15:41:39 +0200

    Do not remove "mssh_copy_id.egg-info" in clean operation
    This directory is used for development environment (pip install -e .[test]).

--- clean.sh
+++ clean.sh
@@ -3,5 +3,5 @@
 SCRIPT_DIR=$(dirname $(readlink -f $0))
 
 cd ${SCRIPT_DIR}
-rm -vrf .coverage .eggs build dist mssh_copy_id.egg-info
+rm -vrf .coverage .eggs build dist
 find . -name '*,cover' -exec rm -vf '{}' \;
