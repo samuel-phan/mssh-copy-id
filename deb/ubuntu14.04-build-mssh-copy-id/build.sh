b4c7d25200a5f8dda18a8f8c9764629223607386
Samuel Phan <1516171+samuel-phan@users.noreply.github.com>
2017-03-24 14:28:18 +0100

    Clean the deb build
    ref #1

--- deb/ubuntu14.04-build-mssh-copy-id/build.sh
+++ deb/ubuntu14.04-build-mssh-copy-id/build.sh
@@ -7,11 +7,7 @@ UIG_GID=$(stat -c '%u:%g' /deb)
 chown -R root:root /deb/*
 
 cd $CONTAINER_DEBBUILD_DIR/mssh-copy-id-*
-export USER=root DEBFULLNAME='Samuel Phan'
-dh_make --createorig -e samuel@quoonel.com -c mit -s -y
-rm -rf debian
-cp -r ../debian .
-dpkg-buildpackage -rfakeroot -us -uc
+dpkg-buildpackage -rfakeroot -b -us -uc
 
 # Revert ownership back of /deb
 chown -R $UIG_GID /deb/*
