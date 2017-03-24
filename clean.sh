abce818592728448ae033ed777c5072cc8110662
Samuel Phan <1516171+samuel-phan@users.noreply.github.com>
2017-03-24 17:23:56 +0100

    Use pyinvoke to handle tasks
    see #4

--- clean.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_DIR=$(dirname $(readlink -f $0))
-
-cd ${SCRIPT_DIR}
-rm -vrf .coverage .eggs build dist
-find . \( -name '*,cover' -o -name '__pycache__' -o -name '*.py[co]' \) -exec rm -vrf '{}' \;
