ab4076d771a7a7f6c6caa40878ef195b7094cde1
Samuel Phan <1516171+samuel-phan@users.noreply.github.com>
2017-05-05 15:34:43 +0200

    Allow the "user" to be root in the container

--- docker/common/sudo-as-user.sh
+++ docker/common/sudo-as-user.sh
@@ -7,5 +7,10 @@
 USER_ID=${LOCAL_USER_ID:-9001}
 
 echo "Starting with UID : $USER_ID"
-useradd --shell /bin/bash -u $USER_ID -o user
-sudo -u user "$@"
+if [ "$USER_ID" -ne "0" ]; then
+    useradd --shell /bin/bash -u $USER_ID -o user
+    echo "user        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
+    sudo -u user "$@"
+else
+    sudo -u root "$@"
+fi
