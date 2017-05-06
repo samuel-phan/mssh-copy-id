fbc8415ec2f2f978a04f633662c34bed3ecc3e09
Samuel Phan <samuel@quoonel.com>
2017-05-06 17:53:49 +0200

    Add first scenario for functional tests
    see #3

--- func-tests/specs/main.spec
+++ func-tests/specs/main.spec
@@ -2,4 +2,9 @@
 
 ## Copy key to one server
 
-* Start sshd "srv1"
+* Start sshd as "sshd"
+* Add user "user" with password "user_password" to container "sshd"
+* Generate SSH keys for "root" in "cli"
+* Run mssh-copy-id on "centos6" as "root" with args "-P user_password user@sshd" using "cli"
+* Test SSH connection on "centos6" as "root" using "cli" to "sshd" as "user"
+* Stop container "sshd"
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 long_description = '''mssh-copy-id is a command-line tool to copy SSH keys to multiple servers.'''
 install_requires = ['paramiko>=1.7', 'argparse']
 dev_require = ['invoke', 'twine']
-tests_require = ['unittest2', 'mock', 'pytest', 'pytest-runner', 'pytest-cov', 'coverage', 'getgauge']
+tests_require = ['unittest2', 'mock', 'pytest', 'pytest-runner', 'pytest-cov', 'coverage', 'getgauge', 'docker']
 
 # Version info -- read without importing
 _locals = {}
