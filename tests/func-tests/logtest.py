4b4e93d79a7166c602d313b909b8f1b5f5804165
Samuel Phan <1516171+samuel-phan@users.noreply.github.com>
2017-08-18 15:37:19 +0200

    Refactor the logs for functional tests
    - have a "[container_name]" prefix for all container logs.
    - use the best practice "%s" for the logs formatting.
    
    see #3

---
+++ tests/func-tests/logtest.py
@@ -0,0 +1,5 @@
+def fmt_ctn_log(ctn_name, msg):
+    """
+    Format container log entry.
+    """
+    return '[{}] {}'.format(ctn_name, msg)
--- tests/func-tests/sshtest.py
+++ tests/func-tests/sshtest.py
@@ -28,7 +28,7 @@ def gen_ssh_keys(ssh_key_dir):
     ssh_pub_file = os.path.join(ssh_key_dir, 'id_rsa.pub')
     filetest.create_dir(ssh_key_dir)
     subprocess.check_call(['ssh-keygen', '-N', "", '-f', ssh_key_file], stdout=open(os.devnull, 'wb'))
-    logger.info('Generated SSH keys "{}".'.format(ssh_key_file))
+    logger.info('Generated SSH keys "%s".', ssh_key_file)
     return SSHKeys(ssh_key_file=ssh_key_file, ssh_pub_file=ssh_pub_file)
 
 
